cbfe1036dc743767071f24b145b026fea9e28c4f
lloydcoder <140634407+LloydCoder@users.noreply.github.com>
2026-08-28 10:43:39 +0100

    test(web): isolate formatting validation

--- app/robots.ts
+++ app/robots.ts
@@ -3,19 +3,7 @@ import { siteConfig } from "@/config/site";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: [
-          "/api/",
-          "/account",
-          "/organizations",
-          "/soc",
-          "/login",
-        ],
-      },
-    ],
+    rules: [{ userAgent: "*", allow: "/" }],
     sitemap: `${siteConfig.url}/sitemap.xml`,
     host: siteConfig.url,
   };
